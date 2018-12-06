5f91a63e23193383e20339fb9388be18594c00d4
Nikolai Wotton <nikolai@gotruemotion.com>
2018-12-06 13:18:46 -0500

    Initial commit

---
+++ app/src/main/java/com/gotruemotion/trueoverflow/MainActivity.kt
@@ -0,0 +1,12 @@
+gitpackage com.gotruemotion.trueoverflow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
