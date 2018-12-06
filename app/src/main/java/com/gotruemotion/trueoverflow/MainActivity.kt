a49ec4a49dda99fb3beada6a750b766f7732a4b3
Nikolai Wotton <nikolai@gotruemotion.com>
2018-12-06 16:00:11 -0500

    Sort options

--- app/src/main/java/com/gotruemotion/trueoverflow/MainActivity.kt
+++ app/src/main/java/com/gotruemotion/trueoverflow/MainActivity.kt
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.SearchView
 import com.gotruemotion.trueoverflow.service.SearchApi
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -17,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private var sortBy = "creation"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             override fun onQueryTextSubmit(query: String?): Boolean =
                 query?.let {
                     NetworkClient.getService(this@MainActivity, SearchApi::class.java)
-                        .search(order = "desc", sort = "creation", query = query)
+                        .search(order = "desc", sort = sortBy, query = query)
                         .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).map { result ->
                             viewAdapter.items.clear()
                             result.items?.let {
@@ -58,5 +61,16 @@ class MainActivity : AppCompatActivity() {
 
             override fun onQueryTextChange(newText: String?): Boolean = false
         })
+        ArrayAdapter.createFromResource(this@MainActivity, R.array.sort_choices, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                sortBy = parent.getItemAtPosition(position) as String
+            }
+        }
     }
 }
